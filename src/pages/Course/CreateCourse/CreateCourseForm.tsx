c0db97e66c8b11daf33853fb32867a722000a863
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2025-09-07 10:51:55 +0600

    create course layout

--- src/pages/Course/CreateCourse/CreateCourseForm.tsx
+++ src/pages/Course/CreateCourse/CreateCourseForm.tsx
@@ -40,7 +40,8 @@ type CourseFormValues = z.infer<typeof createCourseZodSchema>;
 export default function CreateCourseForm() {
   const [preview, setPreview] = React.useState<string | null>(null);
 
-  const [createCourse] = useAddCourseMutation();
+  const [createCourse, { isLoading }] = useAddCourseMutation();
+  // const [createCourse, { isLoading, isSuccess, isError, error }] = useAddCourseMutation();
 
   const form = useForm<CourseFormValues>({
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -91,7 +92,10 @@ export default function CreateCourseForm() {
     try {
       const res = await createCourse(formData).unwrap();
       console.log("res:", res);
-      toast.success("Course Added");
+
+      if (res) {
+        toast.success("Course Added Successfully!");
+      }
 
       form.reset({
         title: "",
@@ -105,7 +109,13 @@ export default function CreateCourseForm() {
         image: undefined,
       });
       setPreview(null);
-    } catch (err) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      if (err?.data?.message) {
+        toast.error(`Something went wrong:${err?.data?.message}`);
+      } else {
+        toast.error("Something went wrong.");
+      }
       console.error(err);
     }
   };
@@ -132,10 +142,10 @@ export default function CreateCourseForm() {
             <h1
               className={cn("text-2xl font-bold dark:text-white text-gray-900")}
             >
-              Create Your Account
+              Create Course
             </h1>
             <p className={cn("text-sm dark:text-gray-300  text-gray-600")}>
-              Fill in the details to get started
+              Fill in the details Here!
             </p>
           </div>
 
@@ -146,176 +156,185 @@ export default function CreateCourseForm() {
                 onSubmit={form.handleSubmit(onSubmit)}
                 className="space-y-6"
               >
-                {/* Title */}
-                <FormField
-                  control={form.control}
-                  name="title"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Title</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Enter course title" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                {/* Description */}
-                <FormField
-                  control={form.control}
-                  name="description"
-                  render={({ field }) => (
-                    <FormItem className="md:col-span-2">
-                      <FormLabel>Description</FormLabel>
-                      <FormControl>
-                        <Textarea
-                          rows={4}
-                          placeholder="Enter course description"
-                          {...field}
-                        />
-                      </FormControl>
-                      {/* <FormDescription>
-                        Keep it under 500 characters.
-                      </FormDescription> */}
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                {/* Category */}
-                <FormField
-                  control={form.control}
-                  name="category"
-                  render={({ field }) => (
-                    <FormItem className="flex-1">
-                      <FormLabel>Category</FormLabel>
-                      <Select
-                        key={field.value || ""}
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
+                {/* Title & Instructor */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                  {/* Title */}
+                  <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-sm font-medium text-center">
+                          Course Title
+                        </FormLabel>
                         <FormControl>
-                          <SelectTrigger className="w-full">
-                            <SelectValue placeholder="Select Category" />
-                          </SelectTrigger>
+                          <Input
+                            placeholder="Enter course title"
+                            className="rounded-xl border border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 transition"
+                            {...field}
+                          />
                         </FormControl>
-                        <SelectContent>
-                          {CourseCategoryOptions.map((item) => (
-                            <SelectItem key={item.value} value={item.value}>
-                              {item.label}
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+                        <FormMessage className="text-red-500 text-xs mt-1" />
+                      </FormItem>
+                    )}
+                  />
 
-                {/* Seat */}
-                <FormField
-                  control={form.control}
-                  name="seat"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Seat</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="number"
-                          //   placeholder="0"
-                          {...field}
-                          value={field.value || ""}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                {/* Price */}
-                <FormField
-                  control={form.control}
-                  name="price"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Price</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="number"
-                          //   placeholder="0"
-                          {...field}
-                          value={field.value || ""}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                {/* Duration */}
-                <FormField
-                  control={form.control}
-                  name="duration"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Duration (hours)</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="number"
-                          //   placeholder="0"
-                          {...field}
-                          value={field.value || ""}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                {/* Instructor */}
-                <FormField
-                  control={form.control}
-                  name="instructor"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Instructor</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Instructor name" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                {/* Status */}
-                <FormField
-                  control={form.control}
-                  name="status"
-                  render={({ field }) => (
-                    <FormItem className="flex-1">
-                      <FormLabel>Status</FormLabel>
-                      <Select
-                        key={field.value || ""}
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
+                  {/* Instructor */}
+                  <FormField
+                    control={form.control}
+                    name="instructor"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-sm font-medium">
+                          Instructor Name
+                        </FormLabel>
                         <FormControl>
-                          <SelectTrigger className="w-full">
-                            <SelectValue placeholder="Select Status" />
-                          </SelectTrigger>
+                          <Input
+                            placeholder="Instructor name"
+                            className="rounded-xl border border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 transition"
+                            {...field}
+                          />
                         </FormControl>
-                        <SelectContent>
-                          {CourseStatusOptions.map((item) => (
-                            <SelectItem key={item.value} value={item.value}>
-                              {item.label}
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+                        <FormMessage className="text-red-500 text-xs mt-1" />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                {/* Category & Status */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                  {/* Category */}
+                  <FormField
+                    control={form.control}
+                    name="category"
+                    render={({ field }) => (
+                      <FormItem className="flex-1">
+                        <FormLabel className="text-sm font-medium">
+                          Category
+                        </FormLabel>
+                        <Select
+                          key={field.value || ""}
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger className="w-full rounded-xl border border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 transition">
+                              <SelectValue placeholder="Select Category" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            {CourseCategoryOptions.map((item) => (
+                              <SelectItem key={item.value} value={item.value}>
+                                {item.label}
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                        <FormMessage className="text-red-500 text-xs mt-1" />
+                      </FormItem>
+                    )}
+                  />
+
+                  {/* Status */}
+                  <FormField
+                    control={form.control}
+                    name="status"
+                    render={({ field }) => (
+                      <FormItem className="flex-1">
+                        <FormLabel className="text-sm font-medium">
+                          Status
+                        </FormLabel>
+                        <Select
+                          key={field.value || ""}
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger className="w-full rounded-xl border border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 transition">
+                              <SelectValue placeholder="Select Status" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            {CourseStatusOptions.map((item) => (
+                              <SelectItem key={item.value} value={item.value}>
+                                {item.label}
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                        <FormMessage className="text-red-500 text-xs mt-1" />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                  {/* Seat */}
+                  <FormField
+                    control={form.control}
+                    name="seat"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-sm font-medium">
+                          Seat
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            type="number"
+                            className="rounded-xl border border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 transition"
+                            {...field}
+                            value={field.value || ""}
+                          />
+                        </FormControl>
+                        <FormMessage className="text-red-500 text-xs mt-1" />
+                      </FormItem>
+                    )}
+                  />
+
+                  {/* Price */}
+                  <FormField
+                    control={form.control}
+                    name="price"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-sm font-medium">
+                          Price
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            type="number"
+                            className="rounded-xl border border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 transition"
+                            {...field}
+                            value={field.value || ""}
+                          />
+                        </FormControl>
+                        <FormMessage className="text-red-500 text-xs mt-1" />
+                      </FormItem>
+                    )}
+                  />
+
+                  {/* Duration */}
+                  <FormField
+                    control={form.control}
+                    name="duration"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-sm font-medium">
+                          Duration (Day's)
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            type="number"
+                            className="rounded-xl border border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 transition"
+                            {...field}
+                            value={field.value || ""}
+                          />
+                        </FormControl>
+                        <FormMessage className="text-red-500 text-xs mt-1" />
+                      </FormItem>
+                    )}
+                  />
+                </div>
 
                 {/* Image */}
                 <FormItem className="md:col-span-2">
@@ -338,16 +357,38 @@ export default function CreateCourseForm() {
                   )}
                 </FormItem>
 
+                {/* Description */}
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem className="md:col-span-2">
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          rows={4}
+                          placeholder="Enter course description"
+                          {...field}
+                        />
+                      </FormControl>
+                      {/* <FormDescription>
+                        Keep it under 500 characters.
+                      </FormDescription> */}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
                 {/*  */}
                 {/* Submit Button */}
                 <Button
                   type="submit"
                   className={cn(
-                    "w-full text-white font-medium py-3 rounded-lg transition-colors dark:bg-violet-700  bg-violet-600 hover:bg-violet-700"
+                    "w-full cursor-pointer text-white font-medium py-3 rounded-lg transition-colors dark:bg-violet-700  bg-violet-600 hover:bg-violet-700"
                   )}
-                  disabled={!form.formState.isValid}
+                  disabled={!form.formState.isValid || isLoading}
                 >
-                  Sign Up
+                  {isLoading ? "Creating.." : "Create"}
                 </Button>
               </form>
             </Form>
